1a09e65eedffd68efe493cc7f8ab99b34f57ae87
umakatestem <itachiuthiwa9@gmail.com>
2026-08-20 02:17:33 +0200

    feat: connexion admin réelle via Firebase Auth + protection des pages + déconnexion

--- public/js/admin/auth-guard.js
+++ public/js/admin/auth-guard.js
@@ -1,8 +1,18 @@
 import { auth } from "../firebase-config.js";
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
+import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
 
 onAuthStateChanged(auth, (user) => {
   if (!user) {
     window.location.href = "login.html";
   }
 });
+
+const logoutBtn = document.getElementById("logoutBtn");
+if (logoutBtn) {
+  logoutBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    signOut(auth).then(() => {
+      window.location.href = "login.html";
+    });
+  });
+}
